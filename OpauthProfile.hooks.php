d7b2a376769fc4c014dec779570ca5f727da311b
Vedmaka <god.vedmaka@gmail.com>
2017-03-25 00:23:17 +0300

    avoid missing index warnings

--- OpauthProfile.hooks.php
+++ OpauthProfile.hooks.php
@@ -20,16 +20,16 @@ class OpauthProfileHooks {
         if( $user ) {
 
             $profile = new OpauthProfile( $user->getId() );
-            $profile->name = $info['name'] ?: null;
-            $profile->email = $info['email'] ?: null;
-            $profile->nickname = $info['nickname'] ?: null;
-            $profile->first_name = $info['first_name'] ?: null;
-            $profile->last_name = $info['last_name'] ?: null;
-            $profile->location = $info['location'] ?: null;
-            $profile->description = $info['description'] ?: null;
-            $profile->image = $info['image'] ?: null;
-            $profile->phone = $info['phone'] ?: null;
-            $profile->url = $info['url'] ?: null;
+            $profile->name = isset($info['name']) ? $info['name'] : null;
+            $profile->email = isset($info['email']) ? $info['email'] : null;
+            $profile->nickname = isset($info['nickname']) ? $info['nickname'] : null;
+            $profile->first_name = isset($info['first_name']) ? $info['first_name'] : null;
+            $profile->last_name = isset($info['last_name']) ? $info['last_name'] : null;
+            $profile->location = isset($info['location']) ? $info['location'] : null;
+            $profile->description = isset($info['description']) ? $info['description'] : null;
+            $profile->image = isset($info['image']) ? $info['image'] : null;
+            $profile->phone = isset($info['phone']) ? $info['phone'] : null;
+            $profile->url = isset($info['url']) ? $info['url'] : null;
             $profile->provider = $provider;
             $profile->uid = $uid;
             $profile->save();
